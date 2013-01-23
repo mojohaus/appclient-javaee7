6a1b7787a4c708106733785defd3e721cfcbbfb4
Milos Kleint <mkleint@gmail.com>
2013-01-23 13:49:12 +0000

    javaee7 archetype prototypes
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/mojo-archetypes/appclient-javaee7@17858 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,15 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+/**
+ * Enterprise Application Client main class.
+ *
+ */
+public class Main {
+    
+    public static void main( String[] args ) {
+        System.out.println( "Hello World Enterprise Application Client!" );
+    }
+}
